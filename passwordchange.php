bb5d212b986b0115ce049ea01ec93fee1655f82c
Rodney_RWR <85048343+Rodney-RWR@users.noreply.github.com>
2022-04-10 16:38:18 +1000

    Update passwordchange.php

--- passwordchange.php
+++ passwordchange.php
@@ -1,187 +1,139 @@
-<?php
-/* --------------------------------------------------------------------
--- Configurable script to change users in htpasswd
------------------------------------------------------------------------
--- This script will change users in an htpasswd file using
--- the PEAR Basic Authentication library. It imports header/footer files
--- if they are found so you can customize the look.
---
--- Installation:
---   Copy this file into a directory that is web protected and
--- update the script to point to the path of your actual htpasswd file.
--- You will need to make the htpasswd file is writable by the web server
--- user.
---
--- Author: Dan Afonso
--- Based on: David A. Horner (DAH) http://dave.thehorners.com
--- License: Public Domain
----------------------------------------------------------------------- */
-// Edit the following to setup your script
-$HTPASSWD_PATH = "/etc/svn/htpasswd";
-$REDIRECT_URL = "/";
-$MIN_PASSLEN = 6;
-$MAX_PASSLEN = -1;
-$HEADER_FILE = 'password_change_header.html';
-$FOOTER_FILE = 'password_change_footer.html';
-// Check for header template, and display it if it's there
-if (is_readable($HEADER_FILE)){
-  $fh = fopen($HEADER_FILE, 'r');
-  fpassthru($fh);
-  fclose($fh);
-} else {
-?>
-<html><head><title>Change Password Script</title></head><body>
-<style>
-  .error_box {
-    font-color: red;
-    margin: 5px;
-    border: thin solid red;
-    background: goldenrod;
-  }
-</style>
-<?php
-}
-if(!include('File/Passwd/Authbasic.php')) {
-?>
-<div class="error_box"><h1>Installation Error</h1>
-Your PEAR configuration is missing some required modules. You can install these
-by running:
-<pre>
-[machine ~]# <b>pear install Auth</b>
-[machine ~]# <b>pear install File_Passwd</b>
-</pre>
-</div>
-<?php
-   exit();
-}
-if(strlen($_SERVER["REMOTE_USER"])==0) {
-?>
-<div class="error_box"><h1>Installation Error</h1>
-This script must be run from a password protected directory on your web site.
-Please visit <a href="http://wiki.apache.org/httpd/PasswordBasicAuth"> the
-Apache Wiki</a> for more information
-</div>
-<?php
-  exit(0);
-}
-/// Check if we can write to the htpasswd file
-if (!is_writable($HTPASSWD_PATH)){
-?>
-<div class="error_box"><h1>Installation Error</h1>
-Your htpasswd file is not writable. You can change this by either:
-<pre>
-[machine ~]> <b>chmod a+w <?php echo $HTPASSWD_PATH; ?></b>
-</pre>
-... or if you prefer more security, as root you can do:
-<pre>
-[machine ~]# <b>chown <?php
-        $a = posix_getpwuid(posix_getuid());
-        echo $a["name"] . ' ' . $HTPASSWD_PATH; ?></b>
-</pre>
-</div>
-<?php
-   exit();
-}
-// Start processing
-$user=$_SERVER["REMOTE_USER"];
-$save_changes=false;
-$passfile = new File_Passwd_Authbasic($HTPASSWD_PATH);
-$ret = $passfile->load();
-if(!PEAR::isError($ret)) {
-  echo "Hello $user, welcome to the password change script.<br>";
-  $verified=false;
-  if(isset($_REQUEST["currpass"]) && strlen($_REQUEST["currpass"])) {
-    // The PEAR library does not handle the (defaut) crypt() password
-    // hash. So we have to decode it manually. Luckally, all the SHA1
-    // hashes start with {SHA1}, and '{' is not present in crypt()
-    // hashes.
-    $users = $passfile->listUser();
-    $salt = substr($users[$_SERVER["REMOTE_USER"]], 0, 2);
-    // Check SHA1 passwords
-    if(isset($users[$_SERVER["REMOTE_USER"]]) &&
-       $salt == '{S' &&
-       $passfile->verifyPasswd($user,$_REQUEST["currpass"])
-      ) {
-      $verified=true;
-      echo "Password validated via SHA1<br/>\n";
-    }
-    // Check crypt()
-    elseif (isset($users[$_SERVER["REMOTE_USER"]]) &&
-            (crypt($_REQUEST["currpass"], $salt) ==
-             $users[$_SERVER["REMOTE_USER"]])) {
-      $verified=true;
-      echo "Password validated via crypt()<br/>\n";
-    } else {
-      echo '<div><b>Failed:</b> Your password did not match </div><br>';
-    }
-   }
-   // Make sure the 2 passwords match
-   $confirmed=false;
-   if(isset($_REQUEST["newpass"]) && strlen($_REQUEST["newpass"]) > $MIN_PASSLEN) {
-      if(strcmp($_REQUEST["newpass"],$_REQUEST["newpass2"]) == 0) {
-         $confirmed=true;
-      } else {
-         echo '<div><b>Failed</b>: new password and confirmed password don\'t match.';
-      }
-   } else if($verified) {
-      echo "<div class=\"error_box\"><b>Failed</b>: you must supply a new password
-             and it must be at least $MIN_PASSLEN characters long.</div>";
-   }
-   if($verified && $confirmed) {
-      $passfile->changePasswd($user, $_REQUEST["newpass"]);
-      $save_changes=true;
-   } else {
-?>
-<form method="post">
-  <table border=0>
-    <tr>
-      <td colspan=2 bgcolor=cccccc>Change your password</td>
-    </tr>
-    <tr>
-      <td>Verify Current Password: </td>
-      <td><input type="password" name="currpass"></td>
-    </tr>
-    <tr>
-      <td>New Password: </td>
-      <td><input type="password" name="newpass"></td>
-    </tr>
-    <tr>
-      <td>New Password Again(to confirm): </td>
-      <td><input type="password" name="newpass2"></td>
-    </tr>
-    <tr>
-      <td colspan=2><input type="submit" name="submit" value="Change Pass">
-      <input type="hidden" name="redirect" value="true"></td>
-    </tr>
-  </table>
-</form>
-<?php
-   }
-   if($save_changes) {
-      echo "Saving password file...<br/>";
-      $ret=$passfile->save();
-      if(!PEAR::isError($ret)) {
-         echo "Password file successfully updated!<br/>";
-         if(isset($_REQUEST["redirect"])) {
-            echo "The browser will request your new username/password in
-                  5 seconds....<br>";
-            echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"5; URL=$REDIRECT_URL\">";
-         }
-      } else {
-         echo '<div><b>Failed</b>: Failed to write password file!<br>';
-         echo "(".$ret->getMessage().")<br></div>";
-      }
-   }
-} else {
-   echo '<div><b>Failed</b>:Failed to open password file!<br>';
-   echo "(".$ret->getMessage().")<br></div>";
-}
-// Display the footer
-if (is_readable($FOOTER_FILE)){
-  $fh = fopen($FOOTER_FILE, 'r');
-  fpassthru($fh);
-  fclose($fh);
-} else {
-  echo "</body></html>";
-}
-?>
+<br/>
+<?<br/>
+///////////////////////////////////////////////////////////////////////<br/>
+// Change Htpasswd Script.<br/>
+///////////////////////////////////////////////////////////////////////<br/>
+// Quick script to change passwords for htpasswd files.<br/>
+//<br/>
+// Installation:<br/>
+//   Simply copy this file into a directory that is web protected and<br/>
+// update the script to point to the path of your actual htpasswd file.<br/>
+// Also, insure that the webserver has read and write permissions.<br/>
+//<br/>
+// Author: David A. Horner (DAH) http://dave.thehorners.com<br/>
+// License: Public Domain<br/>
+///////////////////////////////////////////////////////////////////////<br/>
+if(!include('File/Passwd/Authbasic.php')) {<br/>
+   echo "Error, you must install some files into your pear configuration before continuing.<br>";<br/>
+   echo "At the commandline (you might want to be root, or anyone with access to pear)<br>";<br/>
+   echo "Run the following commands<br>";<br/>
+   echo "pear install Auth<br>";<br/>
+   echo "pear install File_Passwd<br>";<br/>
+   exit();<br/>
+}<br/>
+<br/>
+$HTPASSWD_PATH = "../htpasswd";<br/>
+$REDIRECT_URL = "/";<br/>
+$MIN_PASSLEN = 6;<br/>
+$MAX_PASSLEN = -1;<br/>
+<br/>
+if(strlen($_SERVER["REMOTE_USER"])==0) {<br/>
+   echo "Failed reading username from enviroment!<br>";<br/>
+   echo "This program must be run from within a protected HTTP directory!";<br/>
+   exit(0);<br/>
+}<br/>
+$user=$_SERVER["REMOTE_USER"];<br/>
+$save_changes=false;<br/>
+<br/>
+echo "<html><head><title>Change Password Script</title></head><body>";<br/>
+$passfile = new File_Passwd_Authbasic($HTPASSWD_PATH);<br/>
+$ret = $passfile->load();<br/>
+if(!PEAR::isError($ret)) {<br/>
+<br/>
+   echo "Hello $user, welcome to the password change script.<br>";<br/>
+<br/>
+   $verified=false;<br/>
+   if(strlen($_REQUEST["currpass"])) {<br/>
+      if($passfile->verifyPasswd($user,$_REQUEST["currpass"])) {<br/>
+         $verified=true;<br/>
+      } else {<br/>
+         echo "Failed current password validation!<br>";<br/>
+      }<br/>
+   }<br/>
+   $confirmed=false;<br/>
+   if(strlen($_REQUEST["newpass"])) {<br/>
+      if(strcmp($_REQUEST["newpass"],$_REQUEST["newpass2"])==0) {<br/>
+         $confirmed=true;<br/>
+      } else {<br/>
+         echo "Failed new password and confirmed password don't match!";<br/>
+      }<br/>
+   } else if($verified) {<br/>
+      echo "Failed you must supply a new password!";<br/>
+   }<br/>
+<br/>
+   if($verified &amp;&amp; $confirmed) {<br/>
+      $passfile->changePasswd($user, $_REQUEST["newpass"]);<br/>
+      $save_changes=true;<br/>
+   } else {<br/>
+      echo "<table border=0>";<br/>
+      echo "<tr><td colspan=2 bgcolor=cccccc>Change your password</td></tr>";<br/>
+      echo "<form method=\"post\" action=\"$PHP_SELF\">";<br/>
+      echo "<tr><td>Verify Current Password: </td>";<br/>
+      echo "<td><input type=\"password\" name=\"currpass\"></td></tr>";<br/>
+      echo "<tr><td>New Password: </td>";<br/>
+      echo "<td><input type=\"password\" name=\"newpass\"></td></tr>";<br/>
+      echo "<tr><td>New Password Again(to confirm): </td>";<br/>
+      echo "<td><input type=\"password\" name=\"newpass2\"></td></tr>";<br/>
+      echo "<tr><td colspan=2>";<br/>
+      echo "<input type=\"submit\" name=\"submit\" value=\"Change Pass\">";<br/>
+      echo "<input type=\"hidden\" name=\"redirect\" value=\"true\">";<br/>
+      echo "</td></tr>";<br/>
+      echo "</form>";<br/>
+      echo "</table>";<br/>
+   }<br/>
+<br/>
+/*<br/>
+   if(strlen($_REQUEST["newuser"]) &amp;&amp;  $confirmed) {<br/>
+      $newuser=$_REQUEST["newuser"];<br/>
+      $newpass=$_REQUEST["newpass"];<br/>
+      echo "Adding user $newuser with password $newpass...<br>";<br/>
+      $ret=$passfile->addUser($newuser,$newpass);<br/>
+      if(!PEAR::isError($ret)) {<br/>
+         echo "Added user successfully!<br>";<br/>
+         $save_changes=true;<br/>
+      } else {<br/>
+         echo "Failed to add user to password file!<br>";<br/>
+         echo "(".$ret->getMessage().")<br>";<br/>
+      }<br/>
+   } else {<br/>
+      echo "<table border=0>";<br/>
+      echo "<tr><td colspan=2 bgcolor=cccccc>Add new user</td></tr>";<br/>
+      echo "<form method=\"post\" action=\"$PHP_SELF\">";<br/>
+      echo "<tr><td>New Username: </td>";<br/>
+      echo "<td><input type=\"text\" name=\"newuser\"></td></tr>";<br/>
+      echo "<tr><td>New Password: </td>";<br/>
+      echo "<td><input type=\"password\" name=\"newpass\"></td></tr>";<br/>
+      echo "<tr><td>New Password Again(to confirm): </td>";<br/>
+      echo "<td><input type=\"password\" name=\"newpass2\"></td></tr>";<br/>
+      echo "<tr><td colspan=2>";<br/>
+      echo "<input type=\"submit\" name=\"submit\" value=\"Add User\">";<br/>
+      echo "</td></tr>";<br/>
+      echo "</form>";<br/>
+      echo "</table>";<br/>
+   }<br/>
+*/<br/>
+<br/>
+   if($save_changes) {<br/>
+      echo "Saving password file...<br>";<br/>
+      $ret=$passfile->save();<br/>
+      if(!PEAR::isError($ret)) {<br/>
+         echo "Password file successfully updated!<br>";<br/>
+         if(isset($_REQUEST["redirect"])) {<br/>
+            echo "The browser will request your new username/password in 5 seconds....<br>";<br/>
+            echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"5; URL=$REDIRECT_URL\">";<br/>
+         }<br/>
+      } else {<br/>
+         echo "Failed to write password file!<br>";<br/>
+         echo "(".$ret->getMessage().")<br>";<br/>
+      }<br/>
+   }<br/>
+<br/>
+} else {<br/>
+   echo "Failed to open password file!<br>";<br/>
+   echo "(".$ret->getMessage().")<br>";<br/>
+}<br/>
+echo "</body></html>";<br/>
+   //$users=$passfile->listUser();<br/>
+   //print_r($users);<br/>
+?><br/>
+<br/>
