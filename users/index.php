dba9aa762cf8a7accb4293c287619e7812468e25
Rodney_RWR <rodney.rwr.yt@gmail.com>
2022-05-06 15:19:06 +1000

    Update index.php

--- users/index.php
+++ users/index.php
@@ -21,7 +21,111 @@ echo "<h1> Welcome "  . $username . "</h1>";
     <th>Role</th>
 </tr>
 <tr>
-<tr><td>Asphy</td><td>Owner</td></tr>
+<tr><td>
+    Asphy
+</td><td>
+    Owner
+</td></tr>
+<tr><td>
+    King_Eloy
+</td><td>
+    Owner
+</td></tr>
+<tr><td>
+    Thunder
+</td><td>
+    Manager
+</td></tr>
+<tr><td>
+    LargeDragon
+</td><td>
+    Admin
+</td></tr>
+<tr><td>
+    Qwazimoto
+</td><td>
+    Admin
+</td></tr>
+<tr><td>
+    Rodney_RWR
+</td><td>
+    Head Developer
+</td></tr>
+<tr><td>
+    Ben
+</td><td>
+    Developer
+</td></tr>
+<tr><td>
+    Plazma
+</td><td>
+    Moderator
+</td></tr>
+<tr><td>
+    Randy
+</td><td>
+    Moderator
+</td></tr>
+<tr><td>
+    Turtle
+</td><td>
+    Moderator
+</td></tr>
+<tr><td>
+    Vicmur
+</td><td>
+    Moderator
+</td></tr>
+<tr><td>
+    PopApplenik
+</td><td>
+    Trainee
+</td></tr>
+<tr><td>
+    Crugs
+</td><td>
+    Trainee
+</td></tr>
+<tr><td>
+    Wolfie
+</td><td>
+    Trainee
+</td></tr>
+<tr><td>
+    Crown of England
+</td><td>
+    Builder
+</td></tr>
+<tr><td>
+    Amelia
+</td><td>
+    Builder
+</td></tr>
+<tr><td>
+    Hitman
+</td><td>
+    Designer
+</td></tr>
+<tr><td>
+    Wessel
+</td><td>
+    Helper
+</td></tr>
+<tr><td>
+    Volut
+</td><td>
+    Helper
+</td></tr>
+<tr><td>
+    Reon
+</td><td>
+    Helper
+</td></tr>
+<tr><td>
+    Commander
+</td><td>
+    Helper
+</td></tr>
 </tr>
 </table>
 
