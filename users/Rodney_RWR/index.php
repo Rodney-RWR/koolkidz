8620fee848e0ad87f0b48a827705d187c91dc25d
Rodney_RWR <rodney.rwr.yt@gmail.com>
2022-05-06 14:57:07 +1000

    Update index.php

--- users/Rodney_RWR/index.php
+++ users/Rodney_RWR/index.php
@@ -3,7 +3,7 @@
 <head>
   <title> KoolKidz | Staff </title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="../css/index.css">
+<link rel="stylesheet" href="../../css/index.css">
 </head>
 <body>
 
